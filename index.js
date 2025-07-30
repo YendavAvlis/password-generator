e08776e7896b7f46c4463187aaf9b4acca1bb852
Vadney Da Silva <31402270+YendavAvlis@users.noreply.github.com>
2025-07-30 19:00:30 +0200

    Copy password

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 let characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","0","1","2","3","4","5","6","7","8","9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?","/"]
 
-let passwordOne = document.querySelector('#password-one')
-let passwordTwo = document.querySelector('#password-two')
-let generate = document.querySelector('#generate')
+const passwordOne = document.querySelector('#password-one')
+const passwordTwo = document.querySelector('#password-two')
+const generate = document.querySelector('#generate')
+
+console.log(passwordOne.textContent)
 
 
 generate.addEventListener('click', () => {
@@ -23,14 +25,9 @@ generate.addEventListener('click', () => {
 })
 
 
-passwordOne.addEventListener('click', () => {
-
-    writeClipboardText()
-});
-
-async function writeClipboardText(passwordOne) {
+async function writeClipboardText(password) {
     try {
-        await navigator.clipboard.writeText(passwordOne.value);
+        await navigator.clipboard.writeText(password.textContent);
     } catch (error) {
         console.error(error.message);
     }
